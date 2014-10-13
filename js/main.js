e465b1165d48cb240006144f1d5141c4e637f042
johnsont91 <Cruinex@Johnsons-MacBook-Pro.local>
2014-10-13 19:27:21 -0400

    com
    com

--- js/main.js
+++ js/main.js
@@ -26,6 +26,7 @@ $(function() {
             $('#banner-headline').bigtext();
             $('#attractions').bigtext();
             $('#restaurant-headline').bigtext();
+            $('#nightlife-headline').bigtext();
         }
     });
 });
